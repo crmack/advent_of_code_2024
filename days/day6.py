e94e7bd7bd695444882128bd7266dbab9b5e1d76
Chris Mack <crmack@gmail.com>
2024-12-05 11:52:48 -0500

    Day 6 setup

---
+++ days/day6.py
@@ -0,0 +1,19 @@
+
+
+def part1(input):
+    total = 0
+
+
+    return total
+
+def part2(input):
+    return 0
+
+def run(file_name):
+    with open(file_name, 'r') as file:
+        input = file.read().replace('\n', ' ')
+
+    result1 = part1(input)
+    result2 = part2(input)
+    print(f"Day 6 - Part 1: {result1}")
+    print(f"Day 6 - Part 2: {result2}")
